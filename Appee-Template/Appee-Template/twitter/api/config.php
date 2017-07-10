96c120b8e64ca50528cf728e569960bf2709ef3c
Sc0urg3r <Armad_R@gatech.edu>
2017-07-10 18:18:58 -0400

    Adding Faces

---
+++ Appee-Template/Appee-Template/twitter/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'kKU67Qr3JY6iZUeE9yNfMLfdn');
+    define('CONSUMER_SECRET', 'DRGls4HhRjdKziQUVAA7HDdVFmX6ryls1zoZhJ7xyJFPHFob3p');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '3309463100-rf4Gp82jvGcohnwzp9SekD1qmcV2m557o8vnXWu');
+    define('ACCESS_SECRET', 'lxKASnCaRuR7LVj7sNMchRr2Eh1Nav4QBeVhOHS5Okgjr');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
